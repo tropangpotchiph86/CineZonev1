2df3908e184969860336be5a57d16f2acc4480bd
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-11 14:02:58 +0800

    02displaypopularmovies

--- js/script.js
+++ js/script.js
@@ -32,11 +32,63 @@ function highlighActiveLink() {
   });
 }
 
+//Fetch data from the API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'e76d2381f0382657b3c2960253bbd771';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+//Display Popular Movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  // console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    
+          <a href="movie-details.html?id=${movie.id}">
+
+          ${
+            movie.poster_path
+              ? `<img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+              : `<img
+              src="./images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+          }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+       `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
 function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('shows');
