9836f5a494e0e34b262747e3f0034da686f1c856
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-11 13:32:30 +0800

    Routes and Active Link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+function highlighActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+
+  links.forEach((link) => {
+    const hrefValue = link.getAttribute('href');
+
+    //movie-details
+    if (
+      global.currentPage.includes('movie-details.html') &&
+      hrefValue.includes('index.html')
+    ) {
+      link.classList.add('active');
+      return;
+    }
+
+    //tv-details
+    if (
+      global.currentPage.includes('tv-details.html') &&
+      hrefValue.includes('shows.html')
+    ) {
+      link.classList.add('active');
+      return;
+    }
+
+    if (global.currentPage.includes(hrefValue) && hrefValue !== '/') {
+      link.classList.add('active');
+    }
+  });
+}
+
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+
+  highlighActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
